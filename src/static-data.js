aa5823f44a1e410820955b959da47b6576660d77
Jaouad Eddadsi <jaouad.eddadsi@ensta-bretagne.org>
2020-03-15 16:18:53 +0100

    Make  static-data for devlopement

---
+++ src/static-data.js
@@ -0,0 +1,18 @@
+const item = {
+  logo_url:
+    "https://imgplaceholder.com/100x100/000/fff?text=DEV&font-size=30&font-family=CaviarDreams_Bold",
+  user_name: "The Practical Dev",
+  submit_date: "Sep 10",
+  title: "Learning React? Start Small.",
+  author: "Dave Ceddia",
+  author_img: "https://picsum.photos/seed/picsum/100/100",
+  description:
+    "Can't pry yourself away from the tutorials? the cure is to make tiny little experiment apps",
+  url: "dev.to",
+  comments: 2,
+  replays: 40,
+  likes: 200,
+  messages: 4
+};
+
+export { item };
