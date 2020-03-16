1db1dc59ef77d298d6f336eda54cab667c23f277
Jaouad Eddadsi <jaouad.eddadsi@ensta-bretagne.org>
2020-03-16 09:52:56 +0100

    Finish the Action component

--- src/Action.js
+++ src/Action.js
@@ -1,18 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Action.css";
 
 function Action({ item }) {
   return (
     <div className="actions">
       <ul>
         <li className="comment">
-          <i class="fa fa-comment"></i>
+          <i class="fa fa-comment"></i> <span>{item.comments}</span>
         </li>
         <li className="retweet">
-          <i class="fa fa-retweet"></i>
+          <i class="fa fa-retweet"></i> <span>{item.replays}</span>
         </li>
         <li className="likes">
           <i class="fa fa-heart"></i>
+          <span>{item.likes}</span>
         </li>
         <li className="messages">
           <i class="fa fa-envelope"></i>
